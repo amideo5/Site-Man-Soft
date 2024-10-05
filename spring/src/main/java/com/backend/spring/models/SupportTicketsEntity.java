dac1863fab2f36b60f355f3132b7f268ab7f0a9a
praveen-kishore-pragya <praveen.pragya1@gmail.com>
2024-10-05 18:17:30 +0530

    Added Support Ticket - Entity and Enum

---
+++ spring/src/main/java/com/backend/spring/models/SupportTicketsEntity.java
@@ -0,0 +1,60 @@
+package com.backend.spring.models;
+
+
+import com.backend.spring.enums.SupportTicketStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "support_tickets")
+public class SupportTicketsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "id", nullable = false)
+    private ProjectEntity project;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SupportTicketStatus status;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "resolved_at", nullable = true)
+    private LocalDateTime resolvedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+
+
+
+}
+
